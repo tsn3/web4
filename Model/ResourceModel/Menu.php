82501450cfb18182363d23f9ec8414c86bcef036
tsn3 <tsn3ps@gmail.com>
2021-02-05 19:45:53 +0200

    last

--- Model/ResourceModel/Menu.php
+++ Model/ResourceModel/Menu.php
@@ -9,5 +9,21 @@ class Menu extends \Magento\Framework\Model\ResourceModel\Db\AbstractDb
         $this->_init('menu', 'menu_id');
     }
 
-}
+    protected function _afterLoad(\Magento\Framework\Model\AbstractModel $object)
+    {
+        $id = $this->getIdFieldName('id');
 
+        $select->joinLeft(
+            array('cms_page'=>$this->getTable('cms_page')),
+            'menu_id.menu=page_id.cms_page',
+            array('*');
+
+        return parent::_afterLoad();
+    }
+
+    public function afterSave(\Magento\Framework\DataObject $object)
+    {
+        $this->_afterSave($object);
+    }
+
+}
