b365efe5843db95fe5baaec19d06440d04dcc55f
tsn3 <tsn3ps@gmail.com>
2021-02-23 10:40:16 +0200

    last

--- Block/Links/LinksPages.php
+++ Block/Links/LinksPages.php
@@ -19,10 +19,10 @@ class LinksPages extends Template
 
     public function getLinksCollection()
     {
-        $link = $this->getLayout()->getBlock('cms_page')->getPage()->getId();
+        $sel_page = $this->getLayout()->getBlock('cms_page')->getPage()->getId();
 
         $this->_collection = $this->_linkCollectionFactory->create();
-        $this->_collection->join(['selected_pages' =>'menupage'], '(selected_pages.menu_id=main_table.menu_id)and(selected_pages.page_id='.$link.')', 'page_id');
+        $this->_collection->join(['selected_pages' =>'menupage'], '(selected_pages.menu_id=main_table.menu_id)and(selected_pages.page_id='.$sel_page.')', 'page_id');
         $this->_collection->addFieldToFilter('status', array('eq' => 1));
         $this->_logger->info($this->_collection->getSelect()->__toString());
         return $this->_collection;
